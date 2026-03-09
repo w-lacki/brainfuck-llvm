af4f5dcf0d7abae5281785cc813af99fb24ae2aa
ponktacology <ponktacology@mailfence.com>
2026-03-10 00:49:33 +0100

    add support for while loops

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <fstream>
 #include <iostream>
+#include <stack>
 #include <llvm/Support/raw_ostream.h>
 
-#include "llvm/ADT/APFloat.h"
-#include "llvm/ADT/STLExtras.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/DerivedTypes.h"
@@ -14,18 +13,12 @@
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Verifier.h"
 
-enum Token {
-    tok_eof = -1,
-    tok_right = -2,
-    tok_left = -3,
-    tok_inc = -4,
-    tok_dec = -5,
-    tok_open = -6,
-    tok_close = -7,
-    tok_in = -8,
-    tok_out = -9,
-};
+typedef struct {
+    llvm::BasicBlock *cond_block;
+    llvm::BasicBlock *outer_block;
+} Loop;
 
+std::stack<Loop> loop_stack;
 
 static llvm::LLVMContext *context;
 static llvm::IRBuilder<> *builder;
@@ -78,6 +71,42 @@ void emit_print(llvm::Value *position_ptr, llvm::Type *tape_type, llvm::Value *m
     builder->CreateCall(print_type, printchar.getCallee(), {val_i32}, "putchar()");
 }
 
+void emit_condjump(llvm::Value *position_ptr, llvm::Type *tape_type, llvm::Value *memory_ptr, llvm::Function *main) {
+    llvm::BasicBlock *condition_block = llvm::BasicBlock::Create(*context, "while.cond", main);
+    builder->CreateBr(condition_block);
+    builder->SetInsertPoint(condition_block);
+
+    llvm::Value *position = builder->CreateLoad(llvm::Type::getInt8Ty(*context), position_ptr, "position");
+    llvm::Value *zero = llvm::ConstantInt::get(llvm::Type::getInt8Ty(*context), 0);
+
+    llvm::Value *address = builder->CreateGEP(tape_type,
+                                              memory_ptr,
+                                              {
+                                                  zero,
+                                                  position,
+                                              },
+                                              "address");
+
+    llvm::Value *curr = builder->CreateLoad(llvm::Type::getInt8Ty(*context), address, "current");
+    llvm::Value *condition = builder->CreateICmpEQ(curr, zero, "while.cmp");
+
+    llvm::BasicBlock *body_block = llvm::BasicBlock::Create(*context, "while.body", main);
+    llvm::BasicBlock *outer_block = llvm::BasicBlock::Create(*context, "while.out", main);
+    builder->CreateCondBr(condition, outer_block, body_block);
+    builder->SetInsertPoint(body_block);
+
+    loop_stack.push({
+        .cond_block = condition_block,
+        .outer_block = outer_block
+    });
+}
+
+void emit_loop_end() {
+    Loop loop = loop_stack.top();
+    loop_stack.pop();
+    builder->CreateBr(loop.cond_block);
+    builder->SetInsertPoint(loop.outer_block);
+}
 
 int main() {
     context = new llvm::LLVMContext();
@@ -97,12 +126,19 @@ int main() {
 
     llvm::ArrayType *tape_type = llvm::ArrayType::get(llvm::Type::getInt8Ty(*context), 2000);
     llvm::AllocaInst *tape = builder->CreateAlloca(tape_type, 0, "tape");
+
     llvm::Constant *zero_aggregator = llvm::ConstantAggregateZero::get(tape_type);
     builder->CreateStore(zero_aggregator, tape);
 
     int chr;
     while ((chr = getchar()) != EOF) {
         switch (chr) {
+            case '[':
+                emit_condjump(position, tape_type, tape, main);
+                continue;
+            case ']':
+                emit_loop_end();
+                continue;
             case '+':
                 emit_inc(position, tape_type, tape, 1);
                 continue;
@@ -117,6 +153,7 @@ int main() {
                 continue;
             case '.':
                 emit_print(position, tape_type, tape);
+            default: continue;
         }
     }
 
